15a12978aae4b446634d74b56554335f5b0320d8
Sami <sami.bencheikh@metropolia.fi>
2025-09-14 15:26:04 +0300

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,20 +2,20 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    const conn = await mongoose.connect("mongodb://localhost:27017/web-dev")
-    console.log(`Connected to database`)
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017/week5")
 
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
 }
 
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI)
-
-//     console.log(`MongoDB Connected: ${conn.connection.host}`)
-//   } catch (error) {
-//     console.log(error)
-//     process.exit(1)
-//   }
-// }
-
 module.exports = connectDB
+
+// const connectDB = async () => {
+//     const conn = await mongoose.connect(process.env.MONGO_URI)
+//     console.log(`Connected to database`)
+
+// }
